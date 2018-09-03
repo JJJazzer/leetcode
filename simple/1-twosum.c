e732727bfbe653270b32a96ee53b7be9183560c3
Yu Liu <ilhanwnz@hotmail.com>
2018-09-03 13:23:06 +0800

    twosum

---
+++ simple/1-twosum.c
@@ -0,0 +1,15 @@
+int* twoSum(int* nums, int numsSize, int target) {
+    /* O(n^2) solution ways */
+    int i, j;
+    int *temp = (int*) malloc(sizeof(int) * 2);
+    for (i = 0; i < numsSize; i++) {
+        for (j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                temp[0] = i;
+                temp[1] = j;
+                return temp;
+            }
+        } 
+    }
+    return temp;
+}
